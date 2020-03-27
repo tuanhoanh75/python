4ae83240cb3e7e3a187a10a265d43a4634cbe391
Tuan Ho Anh <tuanhoanh75@gmx.de>
2020-03-27 14:29:43 +0100

    Implemented a Consensus Alogrithm

--- blockchain.py
+++ blockchain.py
@@ -1,13 +1,16 @@
 import hashlib
 import json
+import requests
 from time import time
 from uuid import uuid4
+from urllib.parse import urlparse
 from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
     def __init__(self):
         self.chain = []
+        self.nodes = set()  # hold the list of nodes
         self.current_transactions = []
 
         # Create a genesis Block
@@ -51,6 +54,20 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. Eg. 'http ://192.168.0.5:5000'
+        :return: None
+
+        urlparse(): parse URL into six components (decompose),
+        returning a 6-tuple
+        Each tuple item is a string, possibly empty
+        """
+
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
     @staticmethod
     def hash(block):
@@ -108,6 +125,63 @@ class Blockchain(object):
         guess = f'{last_proof} {proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We are only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if discovered a new, valid chain lobger than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
 
 # Instantiate our Node
